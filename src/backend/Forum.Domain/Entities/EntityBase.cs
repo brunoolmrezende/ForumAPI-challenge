ebca5f8924dd5f61c8f82f6165a8d8a9a068ba50
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-04-02 23:51:56 -0300

    feat: add user entity and dbcontext.

---
+++ src/backend/Forum.Domain/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Forum.Domain.Entities
+{
+    public class EntityBase
+    {
+        public long Id { get; set; }
+        public bool Active { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ src/backend/Forum.Infrastructure/DataAccess/ForumDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Forum.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Infrastructure.DataAccess
+{
+    public class ForumDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ForumDbContext).Assembly);
+        }
+    }
+}
