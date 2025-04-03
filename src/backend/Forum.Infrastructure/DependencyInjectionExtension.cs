647c39f7fc65e745b0167e17e384e61341618993
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-04-02 23:59:12 -0300

    feat: add Infrastructure Dependency Injection Extension for dbContext.

---
+++ src/backend/Forum.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator.Runner;
+using Forum.Infrastructure.DataAccess;
+using Forum.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Forum.Infrastructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext(services, configuration);
+        }
+
+        private static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.ConnectionString();
+
+            services.AddDbContext<ForumDbContext>(dbContextOptions =>
+            {
+                dbContextOptions.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
---
+++ src/backend/Forum.Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Forum.Infrastructure.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static string ConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetConnectionString("ConnectionSQLServer")!;
+        }
+
+        public static bool IsUnitTestEnviroment(this IConfiguration configuration) => configuration.GetValue<bool>("InMemoryTest");
+    }
+}
