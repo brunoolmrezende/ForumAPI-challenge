133cea2597fe28d8d93a96d5e45cd2b481444678
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-04-20 22:25:39 -0300

    feat: add tests for delete user photo.

---
+++ tests/UseCases.Test/User/Delete Image/DeleteImageUseCaseTest.cs
@@ -0,0 +1,52 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Services.Photo;
+using FluentAssertions;
+using Forum.Application.UseCases.User.Delete_Image;
+using Forum.Exceptions;
+using Forum.Exceptions.ExceptionBase;
+
+namespace UseCases.Test.User.Delete_Image
+{
+    public class DeleteImageUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            (var user, _) = UserBuilder.Build();
+
+            var useCase = CreateUseCase(user);
+
+            Func<Task> act = async () => await useCase.Execute();
+
+            await act.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async Task Error_User_Does_Not_Have_Photo()
+        {
+            (var user, _) = UserBuilder.Build();
+            user.ImageIdentifier = null;
+            user.ImageUrl = null;
+
+            var useCase = CreateUseCase(user);
+
+            Func<Task> act = async () => await useCase.Execute();
+
+            await act.Should().ThrowAsync<NotFoundException>()
+                .Where(error => error.GetErrorMessage().Count == 1
+                    && error.GetErrorMessage().Contains(ResourceMessagesException.USER_DOES_NOT_HAVE_PHOTO));
+        }
+
+        private static DeleteImageUseCase CreateUseCase(Forum.Domain.Entities.User user)
+        {
+            var loggedUser = LoggedUserBuilder.Build(user);
+            var userUpdateOnlyRepository = new UserUpdateOnlyRepositoryBuilder().GetById(user).Build();
+            var unitOfWork = UnitOfWorkBuilder.Build();
+            var photoService = new PhotoServiceBuilder().Build();
+
+            return new DeleteImageUseCase(loggedUser, userUpdateOnlyRepository, photoService, unitOfWork);
+        }
+    }
+}
---
+++ tests/WebApi.Test/User/Delete Image/DeleteImageInvalidTokenTest.cs
@@ -0,0 +1,75 @@
+﻿using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens;
+using FluentAssertions;
+using Forum.Exceptions;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.User.Delete_Image
+{
+    public class DeleteImageInvalidTokenTest(CustomWebApplicationFactory factory) : ForumClassFixture(factory)
+    {
+        private readonly string _endpoint = "user/delete-photo";
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Token_Empty(string culture)
+        {
+            var response = await DoDelete(_endpoint, token: string.Empty, culture);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+            using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+            var expectedMessage = ResourceMessagesException.ResourceManager.GetString("NO_TOKEN", new CultureInfo(culture));
+
+            errors.Should().ContainSingle().And.Contain(error => error.GetString()!.Equals(expectedMessage));
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Token_Invalid(string culture)
+        {
+            var response = await DoDelete(_endpoint, token: "invalidToken", culture);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+            using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+            var expectedMessage = ResourceMessagesException.ResourceManager.GetString("USER_WITHOUT_PERMISSION_ACCESS_RESOURCE", new CultureInfo(culture));
+
+            errors.Should().ContainSingle().And.Contain(error => error.GetString()!.Equals(expectedMessage));
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Token_With_User_Not_Found(string culture)
+        {
+            var token = AccessTokenGeneratorBuilder.Build().Generate(Guid.NewGuid());
+
+            var response = await DoDelete(_endpoint, token, culture);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+            using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+            var expectedMessage = ResourceMessagesException.ResourceManager.GetString("USER_WITHOUT_PERMISSION_ACCESS_RESOURCE", new CultureInfo(culture));
+
+            errors.Should().ContainSingle().And.Contain(error => error.GetString()!.Equals(expectedMessage));
+        }
+    }
+}
